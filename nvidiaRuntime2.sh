d643058a342ac2c73162418bb5c99607b83d1367
misiugodfrey <misiug@nvidia.com>
2026-06-10 16:24:30 -0700

    chore: add NVIDIA SPDX headers to 5 source files
    Directly inserts NVIDIA SPDX license headers into files that did not
    receive them in the previous commit (verify-copyright only updates files
    that already have some form of copyright header). Part of org-wide SPDX
    header rollout (see heavydb-internal#8287).

--- nvidiaRuntime2.sh
+++ nvidiaRuntime2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # Script to configure Nvidia drivers and set up Nvidia container toolkit
 
 nvidia_docker_toolkit() {
